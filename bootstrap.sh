246fe42f360c51c6a5c4538fe4d4f7a038fbca08
julencasazk <julencasazk@gmail.com>
2026-02-05 15:12:59 +0100

    Fix wrong path to copy ROS2 messages when bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ declare -a EXTRA_DIRS=(
   "$ROOT/firmware/stm32/platooning_control/micro_ros_stm32cubemx_utils/extra_packages"
   "$ROOT/firmware/stm32/platooning_control/micro_ros_stm32cubemx_utils/microros_static_library/library_generation/extra_packages"
   "$ROOT/firmware/stm32/platooning_control/micro_ros_stm32cubemx_utils/microros_static_library_ide/library_generation/extra_packages"
-  "$ROOT/firmware/esp32/imu/micro_ros_espidf_component/extra_packages"
+  "$ROOT/firmware/esp32/imu/components/micro_ros_espidf_component/extra_packages"
   "$ROOT/firmware/esp32/motor_control/components/micro_ros_espidf_component/extra_packages"
 )
 
